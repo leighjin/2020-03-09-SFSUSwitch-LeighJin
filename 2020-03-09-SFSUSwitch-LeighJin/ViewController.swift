2dbdfa67b507cb15dbf70eb4e1456ce0cc0b701f
Leigh Jin <jinlei@sfsu.edu>
2020-03-09 17:31:26 -0700

    Initial Commit

---
+++ 2020-03-09-SFSUSwitch-LeighJin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2020-03-09-SFSUSwitch-LeighJin
+//
+//  Created by Leigh Jin on 3/9/20.
+//  Copyright © 2020 SFSU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
